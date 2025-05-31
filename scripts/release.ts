a0b650bc9819779c156d4c9b32a4fefa2f0b854e
Frans Ekman <fransedvinekman@gmail.com>
2025-05-31 16:47:12 +0200

    feat: Release script (#6)
    Adds a simple script for creating a release PR

---
+++ scripts/release.ts
@@ -0,0 +1,17 @@
+import { execSync } from "child_process";
+
+const version = process.argv[2];
+
+const chain = (...commands: string[]) => {
+  return commands.join(" && ");
+};
+
+execSync(
+  chain(
+    `git checkout -b release/v${version}`,
+    `pnpm version ${version}`,
+    `git push --set-upstream origin release/v${version}`,
+    `gh pr create --title "Release v${version}" --body "Release v${version}" --label "release"`,
+  ),
+  { stdio: "inherit" },
+);
