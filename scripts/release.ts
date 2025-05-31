efa8bce7f9fa772917aa8ae92e9defd8be9beeaf
Frans Ekman <fransedvinekman@gmail.com>
2025-05-31 16:36:02 +0200

    automate pr creation

--- scripts/release.ts
+++ scripts/release.ts
@@ -11,6 +11,7 @@ execSync(
     `git checkout -b release/v${version}`,
     `pnpm version ${version}`,
     `git push --set-upstream origin release/v${version}`,
+    `gh pr create --title "Release v${version}" --base main --head release/v${version}`,
   ),
   { stdio: "inherit" },
 );
